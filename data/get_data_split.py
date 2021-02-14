368d53458b4660bc0d263d44e2421fce25118062
mega <mega.mor@gmail.com>
2021-02-14 13:19:44 +0200

    split each data file separately to in and out of domain

--- data/get_data_split.py
+++ data/get_data_split.py
@@ -24,45 +24,36 @@ def main(args):
         test_info = json.load(fd)
 
     # create new data split
-    new_train_lines = []
-    new_valid_lines = []
-    new_test_lines = []
-    train_page_count = 0
-    valid_page_count = 0
-    test_page_count = 0
     for split_lines, split_info, split_name in \
             zip([train_lines, valid_lines, test_lines],
                 [train_info, valid_info, test_info],
                 ["train", "valid", "test"]):
+        in_domain_lines = []
+        out_domain_lines = []
+        in_domain_page_count = 0
+        out_domain_page_count = 0
+
         for title, info in tqdm(split_info.items()):
-            # dev set includes only in-domain test examples.
-            if split_name == "valid" and "what" in info and info["what"] == args.split_what:
+            if "what" in info and info["what"] == args.split_what:
                 for line_idx in range(info["start_line_idx"], info["end_line_idx"]+1):
-                    new_valid_lines.append(split_lines[line_idx])
-                valid_page_count += 1
-            # test set includes only in-domain test examples.
-            elif split_name == "test" and "what" in info and info["what"] == args.split_what:
+                    in_domain_lines.append(split_lines[line_idx])
+                in_domain_page_count += 1
+            else:
                 for line_idx in range(info["start_line_idx"], info["end_line_idx"]+1):
-                    new_test_lines.append(split_lines[line_idx])
-                test_page_count += 1
+                    out_domain_lines.append(split_lines[line_idx])
+                out_domain_page_count += 1
 
-            # train set includes out-of-domain train and dev examples + in-domain train examples.
-            elif split_name in ["train", "valid"]:
-                for line_idx in range(info["start_line_idx"], info["end_line_idx"]+1):
-                    new_train_lines.append(split_lines[line_idx])
-                train_page_count += 1
+        # write data files
+        with open(os.path.join(args.output_dir, f"wiki-{args.split_what}-in-domain.{split_name}.tokens"), "w",
+                  encoding='utf8') as fd:
+            fd.writelines(in_domain_lines)
+        with open(os.path.join(args.output_dir, f"wiki-{args.split_what}-out-domain.{split_name}.tokens"), "w",
+                  encoding='utf8') as fd:
+            fd.writelines(out_domain_lines)
 
-    print(f"created a data split for '{args.split_what}': "
-          f"train pages {train_page_count}, valid pages {valid_page_count}, test pages {test_page_count}, "
-          f"train lines {len(new_train_lines)}, valid lines {len(new_valid_lines)}, test lines {len(new_test_lines)}.")
-
-    # write data files
-    with open(os.path.join(args.output_dir, f"wiki-{args.split_what}.train.tokens"), "w", encoding='utf8') as fd:
-        fd.writelines(new_train_lines)
-    with open(os.path.join(args.output_dir, f"wiki-{args.split_what}.valid.tokens"), "w", encoding='utf8') as fd:
-        fd.writelines(new_valid_lines)
-    with open(os.path.join(args.output_dir, f"wiki-{args.split_what}.test.tokens"), "w", encoding='utf8') as fd:
-        fd.writelines(new_test_lines)
+        print(f"wrote {split_name} data files for '{args.split_what}': "
+              f"{len(in_domain_lines)}/{len(out_domain_lines)} in/out-domain lines, "
+              f"{in_domain_page_count}/{out_domain_page_count} in/out-domain pages")
 
 
 if __name__ == '__main__':
